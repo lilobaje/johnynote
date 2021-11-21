eea7e0f4d3a6db262a7ffc2d0973db0d56b188b6
lilobaje <67030349+lilobaje@users.noreply.github.com>
2021-11-21 02:35:04 -0800

    update

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run() #turn of when running in production
+    app.run(debug=True) #turn of when running in production
 
     
